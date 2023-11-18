10119f6ff4f209cd148baf4f10b591318b36f3ae
Ilias Kostidis <kostidisilias@gmail.com>
2023-11-18 21:26:22 +0200

    c4-read and display all items (up to 50) from data.txt)

--- rainfall.f95
+++ rainfall.f95
@@ -5,8 +5,8 @@ integer::rain(5)
 integer,character(*)::town(5)
 character(*)::larissa
 
-rain=(/405,450,920,610,530/)
-town=(/athens,thessaloniki,patra,heraklio,larissa/)
+
+
 
 print*,town 
 print*,rain
